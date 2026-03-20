2facc6e2d367a45fb226bf19c58acebf23f9dcc6
Sindhuja Parthasarathy <sarathy.sindhu@gmail.com>
2026-03-20 09:05:07 +0100

    Fixed bugs in attachments , complete status and due date

--- src/tasks/TaskFormContent.jsx
+++ src/tasks/TaskFormContent.jsx
@@ -32,7 +32,7 @@ const TaskFormContent = ({addTodo,updateTodo,editTodo,setEditTodo}) => {
         ...data,
         id:Date.now(),
         completed:false,
-        attachments: data.attachments?.[0]?.name || null
+        attachments: data.attachments ? data.attachments.length : 0 
       });
     }
     reset();
@@ -43,23 +43,23 @@ const TaskFormContent = ({addTodo,updateTodo,editTodo,setEditTodo}) => {
         <form id="todoForm" onSubmit={handleSubmit(onSubmit)}>
         <div className="mb-3">
           <label htmlFor="txtTitle" className="form-label">Title</label>
-          <input type="text" name="title" className="form-control" id="txtTitle" placeholder="Enter Title" {...register("title",{required:"Title is required"})}/>
+          <input type="text" className="form-control" id="txtTitle" placeholder="Enter Title" {...register("title",{required:"Title is required"})}/>
           <small className="text-danger">{errors.title && errors.title.message}</small>
         </div>
            <div className="mb-3">
           <label htmlFor="txtDescription" className="form-label">Description</label>
-          <textarea className="form-control" name="description" rows="3" id="txtDescription" placeholder="Enter Description" {...register("description",{required:"Description is required"})}></textarea>
+          <textarea className="form-control" rows="3" id="txtDescription" placeholder="Enter Description" {...register("description",{required:"Description is required"})}></textarea>
           <small className="text-danger">{errors.description && errors.description.message}</small>
         </div>
           <div className="row">
             <div className="col-md-6 mb-3">
               <label htmlFor="dueDateInput" className="form-label">Due Date</label>
-              <input type="datetime-local" className="form-control" name="dueDate" id="dueDateInput" {...register("dueDate",{required:"Due date is required"})}/>
+              <input type="datetime-local" className="form-control" id="dueDateInput" {...register("dueDate",{required:"Due date is required",min:{value:new Date().toISOString().substring(0,16),message:"Due date cannot be set in the past"}})}/>
               <small className="text-danger">{errors.dueDate && errors.dueDate.message}</small>
             </div>
             <div className="col-md-6 mb-3">
               <label htmlFor="assignPerson" className="form-label">Assign to Person(Optional)</label>
-              <select className="form-select" id="assignPerson" name="assignPerson" {...register("assignedTo")}>
+              <select className="form-select" id="assignPerson" {...register("assignedTo")}>
                 <option value="0">-- Select Person (optional) -- </option>
                 <option value="Mattias Hellman">Mattias Hellman</option>
                 <option value="Sindhuja Parthasarathy">Sindhuja Parthasarathy</option>
@@ -72,7 +72,7 @@ const TaskFormContent = ({addTodo,updateTodo,editTodo,setEditTodo}) => {
            <div className="mb-3">
               <label htmlFor="fileAttachments" className="form-label me-2">Attachments (Optional)</label>
               <div className="input-group">
-              <input type="file" className="form-control" id="fileAttachments" name="attachements" multiple  {...register("attachments")}/>
+              <input type="file" className="form-control" id="fileAttachments" multiple  {...register("attachments")}/>
               <button  id="clearBtn" className="btn btn-outline-secondary" type="btn-close"><i className="bi bi-x-lg"></i></button>
               </div>
             </div>
--- src/tasks/TodoListContent.jsx
+++ src/tasks/TodoListContent.jsx
@@ -48,20 +48,20 @@ const ListContent = ({todos,deleteTodo,toggleComplete,editTodoList}) => {
                 </div>
                 <div className="col-auto order-2 mb-1 order-sm-3 text-end">
                   <div className="btn-group" aria-label="Edit">
-                    <button type="button"   className={`btn btn-sm ${
+                    <button type="button" disabled={todo.completed}  className={`btn btn-sm ${
                           todo.completed ? "btn-success" : "btn-outline-success"
                         }`} onClick={()=> toggleComplete(todo.id)}>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-check-lg" viewBox="0 0 16 16">
                         <path d="M12.736 3.97a.733.733 0 0 1 1.047 0c.286.289.29.756.01 1.05L7.88 12.01a.733.733 0 0 1-1.065.02L3.217 8.384a.757.757 0 0 1 0-1.06.733.733 0 0 1 1.047 0l3.052 3.093 5.4-6.425z"/>
                       </svg>
                     </button>
-                    <button type="button" className="btn btn-sm btn-outline-primary editBtn" onClick={() => editTodoList(todo)}>
+                    <button type="button" disabled={todo.completed} className="btn btn-sm btn-outline-primary editBtn" onClick={() => editTodoList(todo)}>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-pencil" viewBox="0 0 16 16">
                         <path d="M12.146.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1 0 .708l-10 10a.5.5 0 0 1-.168.11l-5 2a.5.5 0 0 1-.65-.65l2-5a.5.5 0 0 1 .11-.168zM11.207 2.5 13.5 4.793 14.793 3.5 12.5 1.207zm1.586 3L10.5 3.207 4 9.707V10h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.293zm-9.761 5.175-.106.106-1.528 3.821 3.821-1.528.106-.106A.5.5 0 0 1 5 12.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.468-.325"/>
                       </svg>
                     </button>
                    
-                    <button type="button" className="btn btn-sm btn-outline-danger btnDelete" onClick={()=> deleteTodo(todo.id)}>
+                    <button type="button" disabled={todo.completed} className="btn btn-sm btn-outline-danger btnDelete" onClick={()=> deleteTodo(todo.id)}>
                       <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-trash" viewBox="0 0 16 16">
                         <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5m3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0z"/>
                         <path d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1zM4.118 4 4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4zM2.5 3h11V2h-11z"/>
@@ -80,19 +80,24 @@ const ListContent = ({todos,deleteTodo,toggleComplete,editTodoList}) => {
                         Due:<span className="ms-1 text-nowrap todoDueDate">{new Date(todo.dueDate).toLocaleDateString()}</span>
                       </div>
                     </div>
+                    
                     <div className="col-sm-auto mt-2 mt-sm-0 d-flex align-items-center gap-2">
+                      {todo.assignedTo != 0 && 
                       <span className="badge text-bg-info text-white py-1 spanAssignedPerson">
                         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" className="align-text-bottom" viewBox="0 0 16 16">
                           <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6m2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0m4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4m-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10s-3.516.68-4.168 1.332c-.678.678-.83 1.418-.832 1.664z"/>
                         </svg>
                        <span className="todoAssignedPerson">{todo.assignedTo}</span>
                       </span>
+                      }
+                      {todo.attachments > 0  && 
                       <span className="badge text-bg-secondary text-white py-1 spanAttachment">
                         <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" className="align-text-bottom" viewBox="0 0 16 16">
                           <path d="M4.5 3a2.5 2.5 0 0 1 5 0v9a1.5 1.5 0 0 1-3 0V5a.5.5 0 0 1 1 0v7a.5.5 0 0 0 1 0V3a1.5 1.5 0 1 0-3 0v9a2.5 2.5 0 0 0 5 0V5a.5.5 0 0 1 1 0v7a3.5 3.5 0 1 1-7 0z"/>
                         </svg>
-                       <span className="todoAttachement">{todo.attachments}</span> 
+                       <span className="todoAttachement">{todo.attachments}{todo.attachments === 1 ? "attachment" : "attachments"} </span> 
                       </span>
+                      }
                     </div>
                   </div>
                 </div>
